43e3f9d12d863035d638025189baaa0c961c6960
mert sayar <merttsayar@gmail.com>
2024-03-18 05:42:48 +0300

    at once

---
+++ index.js
@@ -0,0 +1,75 @@
+import express from "express";
+import {createServer} from "http";
+import { Server } from "socket.io";
+import { Kafka } from "kafkajs";
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+      origin: process.env.FRONT,
+      methods: ["GET", "POST"]
+    }
+  });
+
+  app.get('/', (req, res) => {
+    res.send("hello")
+  });
+
+const kafka = new Kafka({
+    clientId: 'my-dashboard',
+    brokers: [process.env.KAFKA_BROKER],
+    sasl: {
+      mechanism: "scram-sha-512",
+      username: process.env.UPSTASH_KAFKA_USERNAME,
+      password: process.env.UPSTASH_KAFKA_PASSWORD,
+    },
+    ssl:true,
+  });
+
+  const consumer = kafka.consumer({ groupId: 'test-group' });
+
+  const run = async () => {
+    await consumer.connect();
+    await consumer.subscribe({ topic: 'mytopic', fromBeginning: true });
+    await consumer.subscribe({ topic: 'ProductViewedCountsTopic', fromBeginning: true });
+    await consumer.subscribe({ topic: 'ProductPurchasedCountsTopic', fromBeginning: true });
+    await consumer.subscribe({ topic: 'ProductFavoritedCountsTopic', fromBeginning: true });
+    
+  
+    await consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        const key = message.key ? message.key.toString() : null;
+        const value = JSON.parse(message.value.toString());
+        
+        console.log({
+            topic,
+            key,
+            value
+        });
+
+        io.emit('kafka-message', {
+            topic,
+            key,
+            value
+        });
+      },
+    });
+  };
+
+  run().catch(console.error);
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+httpServer.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ readme.md
@@ -0,0 +1,9 @@
+## Setup
+
+1. Setup https://github.com/hmsayar/spring-kafka-event-generator
+2. Clone the repository.
+3. Create .env file.
+```PORT:3000```
+```KAFKA_BROKER=:your_ip_address_not_localhost:9092```
+```FRONT=:frontend_app```
+4. Run  ```node index.js``
